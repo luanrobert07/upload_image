a0b9cabfbfb60a3b7299a3bd0f849733444f83f7
luanr <luan.robert@gec.inatel.br>
2023-04-24 22:52:27 -0300

    Salvando URL no Banco de dados

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(uploadRouter);
 
-app.listen(8080, () => {
-  console.log('Form running on port 8080');
+app.listen(3000, () => {
+  console.log('Form running on port 3000');
 });
--- router.js
+++ router.js
@@ -3,6 +3,20 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const { google } = require("googleapis");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017");
+
+// conexão com o banco de dados
+const db = mongoose.connection;
+
+// Uma vez conectado retorna isso
+db.once("open", () => {
+  console.log("Connected to database!");
+});
+
+// Se der ruim imprimi isso
+db.on("error", console.error.bind(console, "connection error: "));
 
 const uploadRouter = express.Router();
 const upload = multer();
@@ -14,6 +28,15 @@ const auth = new google.auth.GoogleAuth({
   keyFile: KEYFILEPATH,
   scopes: SCOPES,
 });
+
+// Definindo o schema do arquivo
+const fileSchema = new mongoose.Schema({
+  name: String,
+  url: String,
+});
+
+// Definindo o modelo do arquivo
+const File = mongoose.model("File", fileSchema);
 
 const uploadFile = async (fileObject, newName) => {
   const bufferStream = new stream.PassThrough();
@@ -30,12 +53,12 @@ const uploadFile = async (fileObject, newName) => {
     fields: "id,name",
   });
   console.log(`Uploaded file ${data.name} ${data.id}`);
-  return data.id;
+  return data;
 };
 
 const DEFAULT_FILE_NAME = "my-file.jpg";
 
-let fileId = "";
+let fileData = {};
 
 uploadRouter.post("/upload", upload.any(), async (req, res) => {
   try {
@@ -44,7 +67,12 @@ uploadRouter.post("/upload", upload.any(), async (req, res) => {
     const { body, files } = req;
 
     for (let f = 0; f < files.length; f += 1) {
-      fileId = await uploadFile(files[f], DEFAULT_FILE_NAME);
+      fileData = await uploadFile(files[f], DEFAULT_FILE_NAME);
+      const newFile = new File({
+        name: fileData.name,
+        url: DRIVE_URL_PREFIX + fileData.id,
+      });
+      await newFile.save();
     }
 
     console.log(body);
@@ -55,8 +83,9 @@ uploadRouter.post("/upload", upload.any(), async (req, res) => {
 });
 
 const DRIVE_URL_PREFIX = "https://drive.google.com/uc?export=view&id=";
+
 const getFileUrl = () => {
-  return DRIVE_URL_PREFIX + fileId; //url para a imagem
+  return DRIVE_URL_PREFIX + fileData.id; // url para a imagem
 };
 
 uploadRouter.get("/fileUrl", (req, res) => {
